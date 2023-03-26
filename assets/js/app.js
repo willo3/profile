bee90cbd92eabe171d5a80cdfb60e976bc83cbc0
William Parker <wrparker@gmail.com>
2023-03-26 13:44:05 +0100

    Feat: restarted profile page from scratch - created background effect with hex and light behind that follows pointer

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const light = document.querySelector('.light');
+  const grid = document.querySelector('#hex-grid');
+
+  grid.addEventListener("mousemove", function (e) {
+    light.style.left = `${e.clientX}px`;
+    light.style.top = `${e.clientY}px`;
+  });
+});
