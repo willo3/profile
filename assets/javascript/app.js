02ac014c72a56ce2fad77791a74b76283c73eb0c
William Parker <wrparker@gmail.com>
2023-03-30 22:50:53 +0100

    Feat: Started implementing navbar responsiveness. Started implementing sticky scroll cards to replace smooth scrolling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,8 @@
+//Event listener for glowing light
 document.addEventListener("DOMContentLoaded", function() {
   const light = document.querySelector('.light');
 
+  //Makes light glow follow mouse pointer
   document.addEventListener("mousemove", function (e) {
     light.style.left = `${e.clientX}px`;
     light.style.top = `${e.clientY}px`;
@@ -14,77 +16,18 @@ jQuery(function(){ //Add class to parent element to margin properly
 	jQuery(".hb-xs").parent().addClass("hb-xs-margin");
 })
 
-jQuery(function(){ //Hover color accordingly as icon
-	//Facebook
-	jQuery( ".hb .fa-facebook,.hb .fa-facebook-square").parent().addClass("hb-facebook");
-	jQuery( ".hb.inv .fa-facebook,.hb.inv .fa-facebook-square").parent().addClass("hb-facebook-inv").removeClass("hb-facebook");
+jQuery(function(){ //Hover color as icon
 
 	//Gmail
-	jQuery( ".hb .fa fa-paper-plane,.hb .fa fa-paper-plane-square").parent().addClass("fa fa-paper-plane");
-	jQuery( ".hb.inv .fa fa-paper-plane,.hb.inv .fa fa-paper-plane-square").parent().addClass("fa fa-paper-plane-inv").removeClass("fa fa-paper-plane");
-
-	//Twitter
-	jQuery( ".hb .fa-twitter,.hb .fa-twitter-square").parent().addClass("hb-twitter");
-	jQuery( ".hb.inv .fa-twitter,.hb.inv .fa-twitter-square").parent().addClass("hb-twitter-inv").removeClass("hb-twitter");
-
-	//Google Plus
-	jQuery( ".hb .fa-google-plus,.hb .fa-google-plus-square").parent().addClass("hb-google-plus");
-	jQuery( ".hb.inv .fa-google-plus,.hb.inv .fa-google-plus-square").parent().addClass("hb-google-plus-inv").removeClass("hb-google-plus");
-
-	//Youtube
-	jQuery( ".hb .fa-youtube,.hb .fa-youtube-square, .hb .fa-youtube-play").parent().addClass("hb-youtube");
-	jQuery( ".hb.inv .fa-youtube,.hb.inv .fa-youtube-square, .hb.inv .fa-youtube-play").parent().addClass("hb-youtube-inv").removeClass("hb-youtube");
+	// jQuery( ".hb .fa fa-paper-plane,.hb .fa fa-paper-plane-square").parent().addClass("fa fa-paper-plane");
+	// jQuery( ".hb.inv .fa fa-paper-plane,.hb.inv .fa fa-paper-plane-square").parent().addClass("fa fa-paper-plane-inv").removeClass("fa fa-paper-plane");
 
 	//Linkedin
 	jQuery( ".hb .fa-linkedin,.hb .fa-linkedin-square").parent().addClass("hb-linkedin");
 	jQuery( ".hb.inv .fa-linkedin,.hb.inv .fa-linkedin-square").parent().addClass("hb-linkedin-inv").removeClass("hb-linkedin");
 
-	//Tumblr
-	jQuery( ".hb .fa-tumblr,.hb .fa-tumblr-square").parent().addClass("hb-tumblr");
-	jQuery( ".hb.inv .fa-tumblr,.hb.inv .fa-tumblr-square").parent().addClass("hb-tumblr-inv").removeClass("hb-tumblr");
-
-	//RSS
-	jQuery( ".hb .fa-rss,.hb .fa-rss-square").parent().addClass("hb-rss");
-	jQuery( ".hb.inv .fa-rss,.hb.inv .fa-rss-square").parent().addClass("hb-rss-inv").removeClass("hb-rss");
-
-	//Pinterest
-	jQuery( ".hb .fa-pinterest,.hb .fa-pinterest-square").parent().addClass("hb-pinterest");
-	jQuery( ".hb.inv .fa-pinterest,.hb.inv .fa-pinterest-square").parent().addClass("hb-pinterest-inv").removeClass("hb-pinterest");
-
-	//Vimeo
-	jQuery( ".hb .fa-vimeo-square").parent().addClass("hb-vimeo");
-	jQuery( ".hb.inv .fa-vimeo-square").parent().addClass("hb-vimeo-inv").removeClass("hb-vimeo");
-
 	//Github
 	jQuery( ".hb .fa-github,.hb .fa-github-square, .hb .fa-github-alt").parent().addClass("hb-github");
 	jQuery( ".hb.inv .fa-github,.hb.inv .fa-github-square, .hb.inv .fa-github-alt").parent().addClass("hb-github-inv").removeClass("hb-github");
-
-	//Flickr
-	jQuery( ".hb .fa-flickr").parent().addClass("hb-flickr");
-	jQuery( ".hb.inv .fa-flickr").parent().addClass("hb-flickr-inv").removeClass("hb-flickr");
-
-	//Dropbox
-	jQuery( ".hb .fa-dropbox").parent().addClass("hb-dropbox");
-	jQuery( ".hb.inv .fa-dropbox").parent().addClass("hb-dropbox-inv").removeClass("hb-dropbox");
-
-	//Xing
-	jQuery( ".hb .fa-xing, .hb .fa-xing-square").parent().addClass("hb-xing");
-	jQuery( ".hb.inv .fa-xing,  .hb.inv .fa-xing-square").parent().addClass("hb-xing-inv").removeClass("hb-xing");
-
-	//Skype
-	jQuery( ".hb .fa-skype").parent().addClass("hb-skype");
-	jQuery( ".hb.inv .fa-skype").parent().addClass("hb-skype-inv").removeClass("hb-skype");
-
-	//dribbble
-	jQuery( ".hb .fa-dribbble").parent().addClass("hb-dribbble");
-	jQuery( ".hb.inv .fa-dribbble").parent().addClass("hb-dribbble-inv").removeClass("hb-dribbble");
-
-	//tencent-weibo
-	jQuery( ".hb .fa-tencent-weibo").parent().addClass("hb-tencent-weibo");
-	jQuery( ".hb.inv .fa-tencent-weibo").parent().addClass("hb-tencent-weibo-inv").removeClass("hb-tencent-weibo");
-
-	//tencent-weibo
-	jQuery( ".hb .fa-instragram").parent().addClass("hb-instragram");
-	jQuery( ".hb.inv .fa-instragram").parent().addClass("hb-instragram-inv").removeClass("hb-instragram");
 
 });
