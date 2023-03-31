b25c79a637c6afabda0d3a1cacf677eb15367471
William Parker <wrparker@gmail.com>
2023-03-31 21:48:37 +0100

    Changed picture for 3d effect svg. Implemented JS electric currenteffect behind hex grid

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -44,3 +44,72 @@ toggleBtn.addEventListener('click', function () {
     ? 'fa fa-caret-up'
     : 'fa fa-bars';
 });
+
+
+// Electric Current Effect
+chars = ["⬤"];
+allStreams = [];
+textCol1 = "#030a1a";
+textCol2 = "#030a1a";
+style = 0;
+var i = 0;
+
+/* ----- Settings ----- */
+// Time Delay between String Spawn
+minStringDelay = 100;
+maxStringDelay = 300;
+
+// Nr. of Chars for Strings
+minStringLength = 4;
+maxStringLength = 12;
+
+// Speed of the Char-Strings (Lower = Faster)
+minStringSpeed = 3;
+maxStringSpeed = 5;
+/* ----- Settings END ----- */
+
+// Create a String with random Chars
+function matrixString(){
+  ranTime = Math.floor(Math.random() * maxStringDelay + minStringDelay);
+  ranLen = Math.floor(Math.random() * maxStringLength + minStringLength);
+  leftPos = Math.floor(Math.random() * $("#text-output").width() + 0);
+  // leftPosRounded = Math.round(Math.floor(Math.random() * $("#text-output").width() + 0) / 30) * 30;
+
+  singleStream = [];
+  for(j=0; j<ranLen;j++){
+    ranChar = Math.floor(Math.random() * chars.length + 0);
+    singleStream[j] = chars[ranChar];
+  }
+
+  // 2 different Nodes for the Colors
+  if($(".matrixString").length % 2 == 0){
+    $("#text-output").append("<div style='color:" + textCol1 + "; text-shadow: 0px 0px 15px " + textCol1 + ", 0px 0px 10px " + textCol1 + ", 0px 0px 5px " + textCol1 + "; top:-500px; left:" + leftPos + "px' class='matrixString ms-col1'>" + singleStream.join("") + "</div>");
+  } else {
+    $("#text-output").append("<div style='color:" + textCol2 + "; text-shadow: 0px 0px 15px " + textCol2 + ", 0px 0px 10px " + textCol2 + ", 0px 0px 5px " + textCol2 + "; top:-500px; left:" + leftPos + "px' class='matrixString ms-col2'>" + singleStream.join("") + "</div>");
+  }
+
+  // Remove the finished Strings
+  $('.matrixString').each(function(i, obj) {
+    if($(this).offset().top > 1500){
+      $(this).remove();
+    }
+  });
+
+  // Recursive Function Call with a randomized Delay
+  setTimeout(function() {
+    i++;
+    // Make sure there are not too many Nodes
+    if ($(".matrixString").length < 200) {
+      matrixString();
+    } else {
+      $(".matrixString:lt(2)").remove();
+      matrixString();
+    }
+  }, ranTime);
+
+  // Animate the Strings
+  minStringSpeed2 = minStringSpeed * 10000; maxStringSpeed2 = maxStringSpeed * 10000;
+  ranSpeed = Math.floor(Math.random() * maxStringSpeed2 + minStringSpeed2);
+  $(".matrixString").animate({ "top": "1500px" }, ranSpeed );
+}
+matrixString();
