906d85db580f704a5eb0743ca6800791c189e057
William Parker <wrparker@gmail.com>
2023-04-07 23:31:04 +0100

    Started implementing JS for switching colours on navbar

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+// Navbar
+let sections = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+  sections.forEach(section => {
+    let top = window.scrollY;
+    let offset = section.offsetTop - 150;
+    let height = section.offsetHeight;
+    let id = section.getAttribute('id');
+
+    if(top >= offset && top < offset + height) {
+      navlinks.forEach(links => {
+        links.classList.remove('active');
+        document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+      });
+    };
+  });
+};
+
 // Accordion
 const accordion = document.querySelector(".accordion");
 
