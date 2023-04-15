74d54bc1ebd36f6ee1e472e2dd97bb3b7ac4e87e
William Parker <wrparker@gmail.com>
2023-04-15 13:30:05 +0100

    Fix: restored centred skills section title on desktop while retaining left position on mobile

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,18 +1,18 @@
 (function() {
   document.addEventListener('DOMContentLoaded', () => {
 
-  // // debounce to prevent screen flicker
-  //   function debounce(func, wait) {
-  //     let timeout;
-  //     return function () {
-  //       const context = this,
-  //         args = arguments;
-  //       clearTimeout(timeout);
-  //       timeout = setTimeout(function () {
-  //         func.apply(context, args);
-  //       }, wait);
-  //     };
-  //   }
+  // debounce to prevent screen flicker
+    function debounce(func, wait) {
+      let timeout;
+      return function () {
+        const context = this,
+          args = arguments;
+        clearTimeout(timeout);
+        timeout = setTimeout(function () {
+          func.apply(context, args);
+        }, wait);
+      };
+    }
 
   // Navbar Scroll and Active Colour Switch
   const sections = document.querySelectorAll('section');
@@ -39,7 +39,7 @@
 
   document.addEventListener('DOMContentLoaded', toggleActiveSection);
   activeSection.addEventListener('click', toggleActiveSection);
-  window.onscroll = toggleActiveSection;
+  window.onscroll = debounce(toggleActiveSection, 100);
 
 // Skills menu
 const hexList = document.querySelectorAll('.hex');
