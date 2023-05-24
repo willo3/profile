1cd088a8d957e5a10ba430c534964c6e7ded0cc0
William Parker <wrparker@gmail.com>
2023-05-24 19:56:03 +0100

    Reactivated JS electricity effect

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -296,74 +296,74 @@ toggleSkills(certificatesOption);
   });
 
 //   // Electric Current Effect
-//   chars = ["⬤"];
-//   allStreams = [];
-//   textCol1 = "#030a1e";
-//   textCol2 = "#030a1e";
-//   style = 0;
-//   var i = 0;
+  chars = ["⬤"];
+  allStreams = [];
+  textCol1 = "#030a1e";
+  textCol2 = "#030a1e";
+  style = 0;
+  var i = 0;
 
-//   /* ----- Settings ----- */
-//   // Time Delay between String Spawn
-//   minStringDelay = 200;
-//   maxStringDelay = 350;
+  /* ----- Settings ----- */
+  // Time Delay between String Spawn
+  minStringDelay = 200;
+  maxStringDelay = 350;
 
-//   // Nr. of Chars for Strings
-//   minStringLength = 4;
-//   maxStringLength = 12;
+  // Nr. of Chars for Strings
+  minStringLength = 4;
+  maxStringLength = 12;
 
-//   // Speed of the Char-Strings (Lower = Faster)
-//   minStringSpeed = 3;
-//   maxStringSpeed = 5;
-//   /* ----- Settings END ----- */
+  // Speed of the Char-Strings (Lower = Faster)
+  minStringSpeed = 3;
+  maxStringSpeed = 5;
+  /* ----- Settings END ----- */
 
-//   // Create a String with random Chars
-// // Create a String with random Chars
-// function matrixString() {
-//   ranTime = Math.floor(Math.random() * maxStringDelay + minStringDelay);
-//   ranLen = Math.floor(Math.random() * maxStringLength + minStringLength);
-//   leftPos = Math.floor(Math.random() * $("#text-output").width() + 0);
+  // Create a String with random Chars
+// Create a String with random Chars
+function matrixString() {
+  ranTime = Math.floor(Math.random() * maxStringDelay + minStringDelay);
+  ranLen = Math.floor(Math.random() * maxStringLength + minStringLength);
+  leftPos = Math.floor(Math.random() * $("#text-output").width() + 0);
 
-//   singleStream = [];
-//   for (j = 0; j < ranLen; j++) {
-//     ranChar = Math.floor(Math.random() * chars.length + 0);
-//     singleStream[j] = chars[ranChar];
-//   }
+  singleStream = [];
+  for (j = 0; j < ranLen; j++) {
+    ranChar = Math.floor(Math.random() * chars.length + 0);
+    singleStream[j] = chars[ranChar];
+  }
 
-//   // 2 different Nodes for the Colors
-//   if ($(".matrixString").length % 2 == 0) {
-//     $("#text-output").append("<div style='color:" + textCol1 + "; text-shadow: 0px 0px 15px " + textCol1 + ", 0px 0px 10px " + textCol1 + ", 0px 0px 5px " + textCol1 + "; top:-500px; left:" + leftPos + "px' class='matrixString ms-col1'>" + singleStream.join("") + "</div>");
-//   } else {
-//     $("#text-output").append("<div style='color:" + textCol2 + "; text-shadow: 0px 0px 15px " + textCol2 + ", 0px 0px 10px " + textCol2 + ", 0px 0px 5px " + textCol2 + "; top:-500px; left:" + leftPos + "px' class='matrixString ms-col2'>" + singleStream.join("") + "</div>");
-//   }
+  // 2 different Nodes for the Colors
+  if ($(".matrixString").length % 2 == 0) {
+    $("#text-output").append("<div style='color:" + textCol1 + "; text-shadow: 0px 0px 15px " + textCol1 + ", 0px 0px 10px " + textCol1 + ", 0px 0px 5px " + textCol1 + "; top:-500px; left:" + leftPos + "px' class='matrixString ms-col1'>" + singleStream.join("") + "</div>");
+  } else {
+    $("#text-output").append("<div style='color:" + textCol2 + "; text-shadow: 0px 0px 15px " + textCol2 + ", 0px 0px 10px " + textCol2 + ", 0px 0px 5px " + textCol2 + "; top:-500px; left:" + leftPos + "px' class='matrixString ms-col2'>" + singleStream.join("") + "</div>");
+  }
 
-//   // Remove the finished Strings
-//   $(".matrixString").each(function(i, obj) {
-//     if ($(this).offset().top > ($(document).height() - $(window).height())) {
-//       $(this).remove();
-//     }
-//   });
+  // Remove the finished Strings
+  $(".matrixString").each(function(i, obj) {
+    if ($(this).offset().top > ($(document).height() - $(window).height())) {
+      $(this).remove();
+    }
+  });
 
-//   // Recursive Function Call with a randomized Delay
-//   setTimeout(function() {
-//     i++;
-//     // Make sure there are not too many Nodes
-//     if ($(".matrixString").length < 200) {
-//       matrixString();
-//     } else {
-//       $(".matrixString:lt(2)").remove();
-//       matrixString();
-//     }
-//   }, ranTime);
+  // Recursive Function Call with a randomized Delay
+  setTimeout(function() {
+    i++;
+    // Make sure there are not too many Nodes
+    if ($(".matrixString").length < 200) {
+      matrixString();
+    } else {
+      $(".matrixString:lt(2)").remove();
+      matrixString();
+    }
+  }, ranTime);
 
-//   // Animate the Strings
-//   minStringSpeed2 = minStringSpeed * 10000;
-//   maxStringSpeed2 = maxStringSpeed * 10000;
-//   ranSpeed = Math.floor(Math.random() * maxStringSpeed2 + minStringSpeed2);
-//   $(".matrixString").animate({ "top": "1500px" }, ranSpeed);
-// }
+  // Animate the Strings
+  minStringSpeed2 = minStringSpeed * 10000;
+  maxStringSpeed2 = maxStringSpeed * 10000;
+  ranSpeed = Math.floor(Math.random() * maxStringSpeed2 + minStringSpeed2);
+  $(".matrixString").animate({ "top": "1500px" }, ranSpeed);
+}
 
-//   matrixString();
+  matrixString();
 
   });
 })();
